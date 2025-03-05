c1167712b0cd4afef6ab327fa1e5d9ad69cdf48b
OKEKE FRANCIS <francisokeke099@gmail.com>
2025-03-05 13:57:21 +0100

    latest commit

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
 import { addTestTask } from "./firebaseConfig";
+import TaskManager from "./TaskManager"; // Import TaskManager
 
 function App() {
   useEffect(() => {
     addTestTask();
   }, []);
 
-  return <h1>Firestore Test: Check Console & Firebase</h1>;
+  return (
+    <div>
+      <h1>Firestore Test: Check Console & Firebase</h1>
+      <TaskManager /> {TaskManager.jsx}
+    </div>
+  );
 }
 
 export default App;
